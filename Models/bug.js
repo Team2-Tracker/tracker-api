b709c19a613694a57eebedb1eadcb589702c92d1
Alexander Becker <alexbecker@Alexanders-MacBook-Pro.local>
2022-06-06 13:27:05 -0700

    bug.js update

--- Models/bug.js
+++ Models/bug.js
@@ -1,13 +1,13 @@
-const mongoose = require('./../connection')
+const mongoose = require("./../connection");
 
 const bugSchema = new mongoose.Schema({
-    bugName: String,
-    Issues: String,
-    Priority: Number,
-    timeEstimate: Number,
-    dateDue: Date,
-    createdDate: Date,
-    Assigned: String,
-})
+  bugName: String,
+  Issues: String,
+  Priority: Number,
+  timeEstimate: Number,
+  dateDue: Date,
+  createdDate: Date,
+  Assigned: String,
+});
 
-module.exports = mongoose.model('Bug', bugSchema)
+module.exports = mongoose.model("Bug", bugSchema);
