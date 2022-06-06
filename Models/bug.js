b0418996d7184a7dcd090a307c5a6f05fd4f0d7e
Irais Avila <iraisavila@Iraiss-Air.attlocal.net>
2022-06-06 13:29:17 -0500

    Set up Schema for bug.js

---
+++ Models/bug.js
@@ -0,0 +1,13 @@
+const mongoose = require('./../connection')
+
+const bugSchema = new mongoose.Schema({
+    bugName: String,
+    Issues: String,
+    Priority: Number,
+    timeEstimate: Number,
+    dateDue: Date,
+    createdDate: Date,
+    Assigned: String,
+})
+
+module.exports = mongoose.model('Bug', bugSchema)
