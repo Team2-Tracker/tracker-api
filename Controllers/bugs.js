90b2addcfef0f33052064560e876cc234e0552b0
Irais Avila <iraisavila@Iraiss-Air.attlocal.net>
2022-06-06 15:20:57 -0500

    Added post route

--- Controllers/bugs.js
+++ Controllers/bugs.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bug = require("./../models/bug.js");
 const Bug = require("./../models/bug.js");
 
 const router = express.Router();
@@ -10,5 +11,15 @@ router.get("/", function (req, res) {
     // Return bugs as json
     .then((bugs) => res.status(200).json({ bugs: bugs }));
 });
+
+// POST /bugs
+router.post('/', function(req, res) {
+    // get new bug data
+    const data = req.body
+    // save bugs to db
+    Bug.create(data)
+    // return bug as json
+    .then((bug => res.status(201).json({bug: bug})))
+})
 
 module.exports = router;
