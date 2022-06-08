4c1fd630d73c49b50324e6db6823cd211a26af84
Alexander Becker <alexbecker@Alexanders-MacBook-Pro.local>
2022-06-08 16:25:43 -0700

    corrected .populate call on controllers/users.js

--- Controllers/users.js
+++ Controllers/users.js
@@ -86,14 +86,25 @@ router.patch("/:userId/bugs/:bugsId", (req, res) => {
     req.params.bugsId,
     { user: req.params.userId },
     { new: true }
-  ).then((bug) => {
-    console.log(bug);
-    User.findByIdAndUpdate(
-      req.params.userId,
-      { $push: { bugs: req.params.bugsId } },
-      { new: true }
-    ).then((user) => res.status(200).json({ user: user }));
-  });
+  )
+    .populate("user", ["username", "firstName", "lastName"])
+    .then((bug) => {
+      console.log(bug);
+      User.findByIdAndUpdate(
+        req.params.userId,
+        { $push: { bugs: req.params.bugsId } },
+        { new: true }
+      )
+        .populate("bugs", [
+          "bugName",
+          "issues",
+          "priority",
+          "timeEstimate",
+          "dateDue",
+          "createDate",
+        ])
+        .then((user) => res.status(200).json({ user: user }));
+    });
 });
 
 router.get("/", (req, res) => {
